eee4463eaa4545e64d910467dc2d3b257365ee8c
FANJANDA <1316287154@QQ.COM>
2020-12-21 23:24:40 +0800

    Add files via upload

---
+++ config/base.php
@@ -0,0 +1,115 @@
+<?php return array (
+  'site_name' => 'OneIndex',
+  'site_name_small' => '',
+  'password' => 'Fanjunda7',
+  'style' => 'nexmoe',
+  'onedrive_root' => '/',
+  'except_path' => '',
+  'cache_type' => 'secache',
+  'cache_expire_time' => 10,
+  'cache_refresh_time' => 600,
+  'root_path' => '?',
+  'show' => 
+  array (
+    'stream' => 
+    array (
+      0 => 'txt',
+    ),
+    'image' => 
+    array (
+      0 => 'bmp',
+      1 => 'jpg',
+      2 => 'jpeg',
+      3 => 'png',
+      4 => 'gif',
+    ),
+    'video5' => 
+    array (
+      0 => 'mp4',
+      1 => 'webm',
+      2 => 'mkv',
+    ),
+    'video' => 
+    array (
+    ),
+    'video2' => 
+    array (
+      0 => 'avi',
+      1 => 'mpg',
+      2 => 'mpeg',
+      3 => 'rm',
+      4 => 'rmvb',
+      5 => 'mov',
+      6 => 'wmv',
+      7 => 'asf',
+      8 => 'ts',
+      9 => 'flv',
+    ),
+    'audio' => 
+    array (
+      0 => 'ogg',
+      1 => 'mp3',
+      2 => 'wav',
+    ),
+    'code' => 
+    array (
+      0 => 'html',
+      1 => 'htm',
+      2 => 'php',
+      3 => 'css',
+      4 => 'go',
+      5 => 'java',
+      6 => 'js',
+      7 => 'json',
+      8 => 'txt',
+      9 => 'sh',
+      10 => 'md',
+    ),
+    'doc' => 
+    array (
+      0 => 'csv',
+      1 => 'doc',
+      2 => 'docx',
+      3 => 'odp',
+      4 => 'ods',
+      5 => 'odt',
+      6 => 'pot',
+      7 => 'potm',
+      8 => 'potx',
+      9 => 'pps',
+      10 => 'ppsx',
+      11 => 'ppsxm',
+      12 => 'ppt',
+      13 => 'pptm',
+      14 => 'pptx',
+      15 => 'rtf',
+      16 => 'xls',
+      17 => 'xlsx',
+    ),
+  ),
+  'images' => 
+  array (
+    'home' => false,
+    'public' => false,
+    'exts' => 
+    array (
+      0 => 'jpg',
+      1 => 'png',
+      2 => 'gif',
+      3 => 'bmp',
+    ),
+  ),
+  'offline' => 
+  array (
+    'offline' => false,
+    'online' => false,
+    'upload_path' => '/upload/',
+  ),
+  'client_secret' => 'fuDDY24?skynqZCTO533]|[',
+  'client_id' => '80668bfc-1f16-4b1d-867d-4d30aafceaf1',
+  'redirect_uri' => 'https://oneindex.github.io/',
+  'area' => 'us',
+  'refresh_token' => '0.AAAARi57pavn30mpFoAn_XIiPfyLZoAWHx1Lhn1NMKr86vFxADE.AgABAAAAAAB2UyzwtQEKR7-rWbgdcBZIAQDs_wMA9P96D09qx7x9JPl-iec8EaaYjzNJuZnbqyuzyVi2qGh7UUXl9sE2E1jjTKAgvSAQbPFgoNq3GHXaQT-3IEMdIbuY379N24Q1LQO7OWzpijGs6Eecin0HJN8V1A7ko8c1YbA-r96ADYzhD4wtCtMIRU6ikSPOXNT5c072ufGiNAPuiSurc6StwKQo7eVBxcCSmqkQjCN4rOMuNVQOLEy6BpKYxEkTQbPA0dfkfQJ-E-azxTN5cDSTzEpx-kgRLaXDfW-VslR7SB6qhQJS7e0lYTEiAQmxAVWvqPpdmqILZg_SvB7IeCB4D24F7nveZRffFJDObjUamgZPN00KtuGa8tV_3-CPY71Fg86jon_H1W-sazB4sl9Pai53wF28hsa8XzuhJ0NvoMokXSV3zeTZcQCReUCg16dA5S1rO59ZDAOlieCC-i1dPF_tQEq8S5jkbWV2O5ofD8vUt59F7-oYZ2-2LdzEx70XE7QznE5bfmcHb88JwDLQvtkKhtzoD1XMMntF5syCoZQUxGtA7bSFct2J1CLhvdscmAQNFRbdxMBUccUGWH63UVDgBD1JuLZehWheTn14zmcno2xGNXx8L_MbBZFYHSwp4hUw80B9UmZozRC1cI87v516GilJybix0zSNbUYckGgGidCrWHAPemV80Uj0NdUZomHsEPEbDnRfbkJ2heDeT8YXQuvs0iHE3lYC84na_iZWTzZOINKqRq16DboZegCJDCj0eK2wkG4xVi5WI3P_JH0DK_u1nuOR2NGC0ZUQzdMseYXZQbk1QktgmGdyfyipvg0Q-ZBRGIBBTA_HDhP_HhBvQfT0mbmHWzkwIQ5MXp_zmmU0xxFhyOfGmKNFcTkM8EPGjBcoF1hX_TFFYo0EsGR2CqcYmQ-qRMojexeeWFIRAyjsWXurLQcdgU0MxeDUYgF9OFd21tY74CB7xPaKRCb4JayT7KdxTFyvREHwjATxEIwk-3Bl0BnEdUW63jnzagr47ym_Sb8nh5Oc0242hq74rwNP6eu0QXWlwI5sX-MkdSc82EwDCz_x1yZ05ESbSVGBDbwR55z-wox361P4TclKoiJtMv0',
+  'onedrive_hide' => '',
+  'onedrive_hotlink' => '',
+);
